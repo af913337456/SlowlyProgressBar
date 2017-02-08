3607e37f6beef643f0bd7503241db25addb5955b
LinGuanHong <913337456@qq.com>
2017-02-08 10:17:14 +0800

    添加多一种例子

--- app/src/main/java/com/slowlyprogressbar/MainActivity.java
+++ app/src/main/java/com/slowlyprogressbar/MainActivity.java
@@ -3,6 +3,7 @@ package com.slowlyprogressbar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /** 第一种，first solution */
         slowlyProgressBar =
                 new SlowlyProgressBar
                         (
@@ -31,6 +34,34 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        /** 第二种动画模式，another */
+        /**
+        slowlyProgressBar =
+                new SlowlyProgressBar
+                        (
+                                findViewById(R.id.p)
+                        );
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                slowlyProgressBar.onProgressStart();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                slowlyProgressBar.onProgressChange(newProgress);
+            }
+        });
+        */
         webView.loadUrl("http://www.cnblogs.com/linguanh");
     }
 
